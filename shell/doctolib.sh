663d2b8bcc0020e7a084f1551f8889e449250e4b
Tobi Poel <tobipoel@gmail.com>
2020-06-07 21:54:24 +0200

    renames flaky detector function

--- shell/doctolib.sh
+++ shell/doctolib.sh
@@ -4,7 +4,8 @@ DOCTOLIB_UTILS=~/git/misc/bash-utils
 source "$DOCTOLIB_UTILS/doctolib-utils.sh"
 
 # Flaky detector shortcut
-fl-det() {
+# (flaky test/integration/doctor/desktop/agenda/appointment/modal/booking2_test.rb 266)
+flaky() {
   classline=$(cat $1 | grep '^\s*class.*$')
   classname=$(echo "$classline" | cut -d ' ' -f 2)
   testname=$(awk NR==$2 $1 | sed -E -e "s/[[:space:]']+/_/g" -e 's/^_*it/test/' -e 's/_do$//')
