42fe15451af7b00ab78563f3f9dcc69e93db1370
Tobi Poel <tobipoel@gmail.com>
2020-08-17 11:02:43 +0200

    adds doctolib bookmarks

--- shell/doctolib.sh
+++ shell/doctolib.sh
@@ -11,3 +11,6 @@ flaky() {
   testname=$(awk NR==$2 $1 | sed -E -e "s/[[:space:]']+/_/g" -e 's/^_*it/test/' -e 's/_do$//')
   rake "ci:run_flaky_detector[$classname,$testname]"
 }
+
+alias pug-prs="open https://github.com/doctolib/doctolib/pulls?q=is%3Apr+is%3Aopen+label%3APUG"
+alias pug-board="open https://doctolib.atlassian.net/secure/RapidBoard.jspa?rapidView=201&projectKey=PUG"
